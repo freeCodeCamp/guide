c4c3f74c31e3d22712b7b68f625a7948daada252
Kuriakin Zeng <zeng.kuriakin@gmail.com>
2017-06-24 08:50:16 +0800

    add TopNavBar

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -7,75 +7,31 @@ import { colors } from 'utils/colors';
 import typography from 'utils/typography';
 import { config } from 'config';
 
+import TopNavBar from '../components/TopNavBar.jsx';
+
 // Import styles.
+import 'bootstrap/dist/css/bootstrap.css';
 import 'css/main.css';
 import 'css/github.css';
 
 const { rhythm, adjustFontSizeTo } = typography;
 
 module.exports = React.createClass({
+  getInitialState(){
+    return { isOpened: false }
+  },
   propTypes() {
     return {
       children: React.PropTypes.object
     };
   },
+  handleClick() {
+    this.setState({ isOpened: !this.state.isOpened })
+  },
   render() {
     return (
       <div>
-        <div
-          style={{
-            background: colors.bg,
-            color: colors.fg,
-            marginBottom: rhythm(1.5)
-          }}
-          >
-          <Container
-            style={{
-              maxWidth: 960,
-              paddingLeft: rhythm(3 / 4)
-            }}
-            >
-            <Grid
-              columns={12}
-              style={{
-                padding: `${rhythm(3 / 4)} 0`
-              }}
-              >
-              <Span
-                columns={4}
-                style={{
-                  // Ugly hack. How better to constrain height of div?
-                  height: 24
-                }}
-                >
-                <Link
-                  style={{
-                    textDecoration: 'none',
-                    color: colors.fg,
-                    fontSize: adjustFontSizeTo('25.5px').fontSize
-                  }}
-                  to={prefixLink('/')}
-                  >
-                  {config.siteTitle}
-                </Link>
-              </Span>
-              <Span columns={ 8 } last={ true } >
-                <a
-                  href='https://github.com/freeCodeCamp/guides'
-                  style={{
-                    float: 'right',
-                    color: colors.fg,
-                    textDecoration: 'none',
-                    marginLeft: rhythm(1 / 2)
-                  }}
-                  >
-                  Github
-                </a>
-                <p>In here is where the Search Bar will go</p>
-              </Span>
-            </Grid>
-          </Container>
-        </div>
+        <TopNavBar handleClick={ this.handleClick }/>
         <Container
           style={{
             maxWidth: 960,
