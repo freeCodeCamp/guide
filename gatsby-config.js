5ff1267f32d1d0d9df93ceaca8349504efeeed66
Michael Henderson <mvhenderson.4@icloud.com>
2017-09-26 11:11:40 -0500

    make linter happy

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-sass`,
+      resolve: 'gatsby-plugin-sass',
       options: {
-        precision: 8,
+        precision: 8
       }
     },
     {
