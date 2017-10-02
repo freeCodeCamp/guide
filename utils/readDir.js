1318f70130b01d1c6ed95e42895dbe103347d211
Timo <timogos.k@live.nl>
2017-10-02 11:00:53 +0200

    Remove elcludedDir filtering

--- utils/readDir.js
+++ utils/readDir.js
@@ -2,14 +2,9 @@ const fse = require('fs-extra');
 
 const { isAFileRE, shouldBeIgnoredRE } = require('./regEx.js');
 
-const excludedDirs = [
-  'search'
-];
-
 module.exports = function readDir(dir) {
   return fse
     .readdirSync(dir)
     .filter(item => !isAFileRE.test(item))
-    .filter(dir => !excludedDirs.includes(dir))
     .filter(file => !shouldBeIgnoredRE.test(file));
 };
