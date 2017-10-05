c66d781ec2339df8a19cd5f6f6e602347fa0f6fc
Timo <timogos.k@live.nl>
2017-10-05 23:41:24 +0200

    Clean up

--- utils/loopPages.js
+++ utils/loopPages.js
@@ -2,19 +2,16 @@ const { Observable } = require('rx');
 
 const readDir = require('./readDir');
 
-// dir is the root pages directory
-// handler is a callback that receives
-// the full path to the page parent dir
-// and the name of the current dir
 module.exports = function loopPages(handler, dir = 'src/pages') {
-  return Observable.from(readDir(dir)).flatMap(articleParentDir => {
-    const path = `${dir}/${articleParentDir}`;
-    handler(path, articleParentDir);
+  return Observable.from(readDir(dir))
+    .flatMap(articleParentDir => {
+      const path = `${dir}/${articleParentDir}`;
+      handler(path, articleParentDir);
 
-    if (readDir(path)) {
-      return loopPages(handler, path);
-    }
+      if (readDir(path)) {
+        return loopPages(handler, path);
+      }
 
-    return Observable.of(null);
+      return Observable.of(null);
   });
 };
