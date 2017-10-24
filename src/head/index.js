3102a18f65dd064424e9ac9b8b49fd0453de3e83
WvRrrr <33044400+WvRrrr@users.noreply.github.com>
2017-10-24 03:07:36 +0200

    Added text
    Line 9

--- src/head/index.js
+++ src/head/index.js
@@ -6,3 +6,4 @@ const metaAndStyleSheets = meta.concat(favicons, styleSheets)
   .map((element, i) => ({ ...element, key: i }));
 
 export default metaAndStyleSheets;
+I want to get a Hacktoberfest shirt! And in a few years I will try my skills in a Hackathon.
