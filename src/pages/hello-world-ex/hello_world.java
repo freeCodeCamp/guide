ba12c036f72241501f73faf370967613f6a48f6b
44758073 <a>
2018-10-06 09:49:45 +1000

    hello_world.java example

---
+++ src/pages/hello-world-ex/hello_world.java
@@ -0,0 +1,11 @@
+/*
+* This is an example of a hello world program in Java
+*/
+
+public class HelloWorld { // create a class that is appropriatly named for our program
+
+    public static void main(String[] args) { 
+        System.out.println("Hello, World"); // our code to print out the string :)
+    }
+
+}
